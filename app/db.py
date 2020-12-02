2b1b2b475ff1740abc4670918f91338bb3e86e94
Ryan Herr <rrherr@gmail.com>
2020-12-02 10:28:35 -0600

    Add get_db function

--- app/db.py
+++ app/db.py
@@ -3,22 +3,38 @@
 import os
 
 from dotenv import load_dotenv
-from fastapi import APIRouter
-from sqlalchemy import create_engine
+from fastapi import APIRouter, Depends
+import sqlalchemy
 
-load_dotenv()
-database_url = os.getenv('DATABASE_URL', default='sqlite:///temporary.db')
-engine = create_engine(database_url)
 router = APIRouter()
 
 
+async def get_db() -> sqlalchemy.engine.base.Connection:
+    """Get a SQLAlchemy database connection.
+    
+    Uses this environment variable if it exists:  
+    DATABASE_URL=dialect://user:password@host/dbname
+
+    Otherwise uses a SQLite database for initial local development.
+    """
+    load_dotenv()
+    database_url = os.getenv('DATABASE_URL', default='sqlite:///temporary.db')
+    engine = sqlalchemy.create_engine(database_url)
+    connection = engine.connect()
+    try:
+        yield connection
+    finally:
+        connection.close()
+
+
 @router.get('/info')
-async def get_url():
+async def get_url(connection = Depends(get_db)):
     """Verify we can connect to the database, 
-    and return the database URL, in this format:
+    and return the database URL in this format:
 
     dialect://user:password@host/dbname
+
+    The password will be hidden with ***
     """
-    with engine.connect() as con:
-        url_without_password = con.engine.url.__repr__()
-        return {'url': url_without_password}
+    url_without_password = repr(connection.engine.url)
+    return {'url': url_without_password}
